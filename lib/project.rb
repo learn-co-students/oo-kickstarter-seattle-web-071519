cdae8a9a467316820d89ea7f1eb574b7cd4db4f2
marshallhelenm <marshallhelenm@gmail.com>
2019-07-18 15:35:08 -0700

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,45 @@
+require "pry"
+
+class Project
+    attr_accessor :title, :backers
+
+    def initialize(title)
+        @title = title
+        @backers = []
+    end
+
+    def backers
+        Backer.all.select do |backer|
+            backer.backed_projects.include?(self)
+            # binding.pry
+        end
+
+    end
+
+    def to_s
+        @title
+    end
+
+    def add_backer(backer)
+        backers << backer
+        backer.backed_projects << self
+        
+    end
+
+end
+
+
+
+
+
+# project1 = Project.new("Awesome Project")
+# project2 = Project.new("Radical Project")
+
+# backer1 = Backer.new("Fred")
+# backer1.back_project(project1)
+# backer2 = Backer.new("George")
+# backer2.back_project(project1)
+# backer2.back_project(project2)
+
+
+# puts project1.backers
